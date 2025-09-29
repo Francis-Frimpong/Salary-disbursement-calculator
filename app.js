94c67d790d64e25f4938eb5ab834e13594b70ff2
Francis <brakay1136@gmail.com>
2025-09-29 09:14:35 -0700

    classes created

---
+++ app.js
@@ -0,0 +1,67 @@
+class CallJsonData {
+  constructor(data) {
+    this.data = data;
+  }
+  async getJson() {
+    const response = await axios.get(this.data);
+    return response.data;
+  }
+}
+
+class CDBCalculator {
+  constructor() {
+    this.tableBody = document.querySelector("tbody");
+  }
+
+  createTableDate() {
+    const getdata = new CallJsonData("/data.json");
+    getdata.getJson().then((res) => {
+      res.forEach((data) => {
+        // console.log(`${data.name}: Gh${data.baseIncome}`);
+        const tr = document.createElement("tr");
+
+        // Name cell
+        const nameTd = document.createElement("td");
+        nameTd.textContent = data.name;
+
+        // Base income cell
+        const incomeTd = document.createElement("td");
+        incomeTd.textContent = `Gh${data.baseIncome}`;
+
+        // Input number cell
+        const inputTd = document.createElement("td");
+        const inputNumber = document.createElement("input");
+        inputNumber.type = "number";
+        inputNumber.placeholder = "Enter value";
+        inputTd.appendChild(inputNumber);
+
+        // Display balance cell
+        const balanceTd = document.createElement("td");
+        const inputText = document.createElement("input");
+        inputText.type = "text";
+        inputText.placeholder = "Display Balance";
+        inputText.readOnly = true;
+        balanceTd.appendChild(inputText);
+
+        // Append all cells to row
+        tr.appendChild(nameTd);
+        tr.appendChild(incomeTd);
+        tr.appendChild(inputTd);
+        tr.appendChild(balanceTd);
+
+        // Append row to tbody
+        this.tableBody.appendChild(tr);
+      });
+    });
+  }
+
+  calculateIncome(baseIncome, cashField, balanceField) {
+    cashField.addEventlister("input", () => {
+      let calculate = baseIncome - +cashField.value;
+      return (balanceField.value = calculate);
+    });
+  }
+}
+
+const calculate = new CDBCalculator();
+calculate.createTableDate();
